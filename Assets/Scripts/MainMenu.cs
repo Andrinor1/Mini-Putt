d4d10dcd2140b4bdaa04986fc5f908b44bfc088a
Joseph Y <jcy_y2000@hotmail.com>
2023-03-23 15:10:06 -0500

    Updated project to include more base-project files, such as TextMesh Pro. Also updated the .gitignore file to ignore UserSettings too (hopefully this works, sometimes it doesn't).

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject menu;
+
+    void Play()
+    {
+        // Just loads the 1st level. This is just placeholder code for now.
+        SceneManager.GetSceneAt(1);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
