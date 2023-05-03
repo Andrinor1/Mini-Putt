fb25e12dba55f99361fc78a6c822f7b181651371
jcy2000 <jcy_y2000@hotmail.com>
2023-05-03 02:07:41 -0500

    Direction indicator now in ball prefab instead of it's own prefab. Deleted triangle prefab. arrowDrag script now on Ball instead of direction indicator.

--- Assets/Scripts/arrowDrag.cs
+++ Assets/Scripts/arrowDrag.cs
@@ -1,68 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class arrowDrag : MonoBehaviour
 {
-    public GameObject ball;
-    private Rigidbody2D ballRigidBody;
-    private ClickAndDrag clickAndDragScript;
-    private CircleCollider2D circleCollider;
+    public GameObject directionArrow;
+    public Rigidbody2D ballRigidBody;
+    public ClickAndDrag clickAndDragScript;
+    public CircleCollider2D circleCollider;
     private bool ballPressed = false;
-    Vector2 mousePosition;
-
-    public void Start()
-    {
-        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        ballRigidBody = ball.GetComponent<Rigidbody2D>();
-        clickAndDragScript = ball.GetComponent<ClickAndDrag>();
-        circleCollider = ball.GetComponent<CircleCollider2D>();
-    }
 
     public void OnMouseDown()
     {
-        //Debug.Log("ballPressed: " + ballPressed);
-        //if (!ballPressed)
-        //{
-        //    // Getting the position of the mouse in terms of game units
-        //    Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        //    // Finding the difference in positions between the ball and the mouse
-        //    Vector2 dir = ballRigidBody.position - mousePosition;
-        //    Debug.Log("dir: " + dir);
-        //    Debug.Log("dir.magnitude: " + dir.magnitude);
-
-        //    if (dir.magnitude <= circleCollider.radius)
-        //    {
-        //        ballPressed = true;
-        //    }
-        //}
+        if (!ballPressed)
+        {
+            ballPressed = true;
+            directionArrow.SetActive(true);
+        }
     }
 
     public void OnMouseUp()
     {
         if (ballPressed)
+        {
             ballPressed = false;
+            directionArrow.SetActive(false);
+        }
     }
 
     private void Update()
     {
-        if (clickAndDragScript.ballStopped && Input.GetMouseButton(0))
+        if (clickAndDragScript.ballStopped && Input.GetMouseButton(0) && ballPressed)
         {
-            Debug.Log("This hsould work");
-            gameObject.SetActive(true);
             // Getting the position of the mouse in terms of game units
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
             // Finding the difference in positions between the ball and the mouse
-            Vector2 dir = ballRigidBody.position - mousePosition;
+            Vector2 positionDifference = ballRigidBody.position - mousePosition;
 
-            // Setting the position of the triangle to that opposite of the ball
-            transform.position = ballRigidBody.position + dir;
-
-            // Angle of the ball is now that of what is calculated. This, in term, rotates the triangle
-            var angle = 180 - Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+            // Angle of the ball is now that of what is calculated. This, in term, rotates the DirectionArrow
+            var angle = 180 - Mathf.Atan2(positionDifference.x, positionDifference.y) * Mathf.Rad2Deg;
             ballRigidBody.rotation = angle;
+
+            // If mouse position is too close to ball, then change positionDifference so that arrow is not too close
+            if (positionDifference.magnitude < circleCollider.radius * 2)
+                positionDifference = positionDifference.normalized * circleCollider.radius * 2;
+
+            // Setting the position of the directionArrow such that it's opposite of the ball
+            directionArrow.transform.position = ballRigidBody.position + positionDifference;
         }
     }
 }
